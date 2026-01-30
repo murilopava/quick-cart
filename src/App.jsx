cb1246b8218f7997faf5c0ba9d09a95dfdad5272
Murilo Pavanello <murilopava@gmail.com>
2026-01-30 20:28:25 -0300

    feat: adicionar função de adicionar e remover itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,48 @@
+import { useRef, useState } from "react";
 import ItemLista from "./itemLista";
 
 function App() {
-  const listaMercado = ["Banana", "Maçã", "Laranja", "Ovo"];
+  // let listaMercado = ["Banana", "Maçã", "Laranja", "Ovo"];
+  const [listaMercado, setListaMercado] = useState([]);
+
+  const inputAdicionar = useRef(); 
+  // console.log(inputAdicionar.current);
+
+  // Hook
+  // useState - Cria uma variável de estado
+  // useRef - Cria uma referência para um elemento do DOM
+
+  const adicionarElementoNaLista = () => {
+    const novaLista = [...listaMercado];
+    const valorInput = inputAdicionar.current.value;
+
+    if (valorInput !== "") {
+      novaLista.push(valorInput);
+      setListaMercado(novaLista);
+  
+      inputAdicionar.current.value = "";
+    }
+
+
+  }
 
   return (
     <>
       <h1>Lista de Mercado</h1>
-      <input type="text" placeholder="Digite um item"/>
-      <button>Adicionar</button>
+      <input ref={inputAdicionar} type="text" placeholder="Digite um item"/>
+      <button onClick={() => adicionarElementoNaLista()}>Adicionar</button>
 
       
-      <ul>
-        {listaMercado.map((itemLista, index) => 
-        <ItemLista key={index} itemLista={itemLista}/>
+      {listaMercado.length > 0 ? (
+        <ul>
+        {listaMercado.map((itemLista, index) => (
+        <ItemLista key={index} itemLista={itemLista} listaMercado={listaMercado} setListaMercado={setListaMercado}/>
+        ))}
+        </ul>
+      ) : (
+        <p>Lista Vazia</p>
       )}
-      </ul>
+      
     </>
   )
 
--- src/ItemLista.jsx
+++ src/ItemLista.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const ItemLista = ({itemLista}) => {
+const ItemLista = ({itemLista, listaMercado, setListaMercado}) => {
   
+    const removerItemDaLista = () => {
+      const novaLista = [...listaMercado];
+      const novaListaFiltrada = novaLista.filter(itemAtual => itemAtual !== itemLista);
+      setListaMercado(novaListaFiltrada);
+    }
     return (
     <>
     <li>
       <p>{itemLista}</p>
-      <button>Remover</button>
+      <button onClick={() => removerItemDaLista()}>Remover</button>
     </li>
     </>
   )
